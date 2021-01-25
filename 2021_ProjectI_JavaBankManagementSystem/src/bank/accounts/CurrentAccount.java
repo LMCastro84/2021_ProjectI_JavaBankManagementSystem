c1990c2c1adb57ab4cccbdc274aac6db24e122a1
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-01-25 22:54:52 +0000

    variable + comment

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/accounts/CurrentAccount.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/accounts/CurrentAccount.java
@@ -1,9 +1,16 @@
 package bank.accounts;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Castro
  */
 public class CurrentAccount extends Account {
+
+    /**
+     * account's list of cards
+     */
+    private ArrayList<Card> cardsList;
 
 }
