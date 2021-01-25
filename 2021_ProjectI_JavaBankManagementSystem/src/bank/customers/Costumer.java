987365159af64753ef8e55b6844511021d12b0a9
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-01-25 22:42:50 +0000

    comments

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/customers/Costumer.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/customers/Costumer.java
@@ -8,13 +8,48 @@ import java.util.ArrayList;
  */
 public class Costumer {
 
+    /**
+     * unique id customer
+     */
     private static int id = 0;
+
+    /**
+     * customer's name
+     */
     private String name;
+
+    /**
+     * customer's phone'
+     */
     private String phone;
+
+    /**
+     * customer's email
+     */
     private String email;
+
+    /**
+     * customer's profession
+     */
     private String profession;
+
+    /**
+     * customer's current account
+     */
     private Account currentAccount;
+
+    /**
+     * customer's list of accounts
+     */
     private ArrayList<Account> accountsList;
+
+    /**
+     * customer's debit card
+     */
     private Card debitCard;
+
+    /**
+     * customer's list of cards
+     */
     private ArrayList<Card> cardsList;
 }
