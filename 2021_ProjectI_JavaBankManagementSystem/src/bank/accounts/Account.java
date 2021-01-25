4c10659484159ed96fbb71b22dec55eb8fb4e395
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-01-25 22:51:10 +0000

    variables + comments

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/accounts/Account.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/accounts/Account.java
@@ -1,9 +1,20 @@
 package bank.accounts;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Castro
  */
 public class Account {
+
+    /**
+     * unique id card
+     */
+    private static int id = 0;
     
+    /**
+     * account's balance
+     */
+    private double balance;
 }
