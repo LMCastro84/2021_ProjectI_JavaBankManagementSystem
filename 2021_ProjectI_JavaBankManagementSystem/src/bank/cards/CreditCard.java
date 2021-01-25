5d3a6e77fab68448fbec302d73bce37c4a19ca08
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-01-25 23:30:08 +0000

    Contructor

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/cards/CreditCard.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/cards/CreditCard.java
@@ -1,10 +1,15 @@
 package bank.cards;
 
+import bank.customers.Costumer;
+
 /**
  *
  * @author Castro
  */
 public class CreditCard extends DebitCard {
-    
+
+    public CreditCard(Costumer costumer) {
+        super(costumer);
+    }
 
 }
