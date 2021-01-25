b7f55047ba84d17421882cc98df334d84ad70fd9
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-01-25 22:39:56 +0000

    variables

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/customers/Costumer.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/customers/Costumer.java
@@ -1,9 +1,20 @@
 package bank.customers;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Castro
  */
 public class Costumer {
 
+    private static int id = 0;
+    private String name;
+    private String phone;
+    private String email;
+    private String profession;
+    private Account currentAccount;
+    private ArrayList<Account> accountsList;
+    private Card debitCard;
+    private ArrayList<Card> cardsList;
 }
