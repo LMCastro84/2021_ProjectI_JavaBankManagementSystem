fad905cb7b854f2ca395b62edd6c607fff20d103
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-01-25 23:17:27 +0000

    variables + comments

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/cards/Card.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/cards/Card.java
@@ -1,9 +1,27 @@
 package bank.cards;
 
+import bank.accounts.Account;
+import bank.customers.Costumer;
+import java.util.ArrayList;
+
 /**
  *
  * @author Castro
  */
 public class Card {
 
+    /**
+     * unique id card
+     */
+    private static int id = 0;
+
+    /**
+     * card's owner
+     */
+    private Costumer costumer;
+
+    /**
+     * card's list of accounts
+     */
+    private ArrayList<Account> accountsList;
 }
