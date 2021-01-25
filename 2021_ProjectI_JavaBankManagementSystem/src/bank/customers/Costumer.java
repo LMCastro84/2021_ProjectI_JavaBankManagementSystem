ce563033dc97c9cdb1506fd372486c6d190f5676
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-01-25 23:11:00 +0000

    Constructor

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/customers/Costumer.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/customers/Costumer.java
@@ -55,12 +55,12 @@ public class Costumer {
      * customer's list of cards
      */
     private ArrayList<Card> cardsList;
-    
+
     CurrentAccount currAccount = new CurrentAccount();
     DebitCard debCard = new DebitCard();
 
     public Costumer(String name, String phone, String email, String profession) {
-        id ++;
+        id++;
         this.name = name;
         this.phone = phone;
         this.email = email;
@@ -70,6 +70,5 @@ public class Costumer {
         this.debitCard = debCard.newDebitCard();
         cardsList.add(debitCard);
     }
-    
-    
+
 }
