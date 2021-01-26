4cb4b868e7cbfcc25eb6c7d0aa714dffca5dfaf0
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-01-26 17:58:57 +0000

    Costumer costumer + comments; constructors + comments

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/accounts/Account.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/accounts/Account.java
@@ -1,5 +1,6 @@
 package bank.accounts;
 
+import bank.customers.Costumer;
 import java.util.ArrayList;
 
 /**
@@ -12,9 +13,29 @@ public class Account {
      * unique id card
      */
     private static int id = 0;
-    
+
     /**
      * account's balance
      */
     private double balance;
+
+    /**
+     * owner of account
+     */
+    private Costumer costumer;
+
+    public Account() {
+    }
+
+    /**
+     * constructor
+     *
+     * @param costumer the owner of the account
+     */
+    public Account(Costumer costumer) {
+        this.id++;
+        this.balance = 0;
+        this.costumer = costumer;
+    }
+
 }
