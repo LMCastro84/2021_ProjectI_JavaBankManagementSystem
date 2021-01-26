759874d8f5830a69537f63d8cd9f68aab102a7f4
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-01-26 23:09:36 +0000

    testing creation of a new costumer

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/Main.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/Main.java
@@ -1,5 +1,9 @@
 package bank;
 
+import bank.accounts.CurrentAccount;
+import bank.cards.DebitCard;
+import bank.customers.Costumer;
+
 /**
  *
  * @author Castro
@@ -7,6 +11,14 @@ package bank;
 public class Main {
 
     public static void main(String[] args) {
+        Costumer cost1 = new Costumer("Castro", "936960352", "castro@gmail.com", "Java Dev");
+        CurrentAccount currAcct = new CurrentAccount(cost1);
+        DebitCard dbtCard = new DebitCard(cost1);
+        cost1.setCurrentAccount(currAcct);
+        cost1.setDebitCard(dbtCard);
+        currAcct.setDebitCard(dbtCard);
+        dbtCard.setCurrentAccount(currAcct);
 
+        cost1.showClientInfo();
     }
 }
