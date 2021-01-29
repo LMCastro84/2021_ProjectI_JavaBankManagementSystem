2e9eb0c40caa0437fac5d4db2216b23cff065f90
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-01-29 23:24:18 +0000

    interest + comment; constructor + comments

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/accounts/SavingsAccount.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/accounts/SavingsAccount.java
@@ -1,9 +1,27 @@
 package bank.accounts;
 
+import bank.customers.Customer;
+
 /**
  *
  * @author Castro
  */
-public class SavingsAccount {
+public class SavingsAccount extends Account {
+
+    /**
+     * interest to be setted when credit card is created
+     */
+    private double interest;
+
+    /**
+     * constructor
+     *
+     * @param costumer the owner of the account
+     * @param interest of the savings account
+     */
+    public SavingsAccount(Customer costumer, double interest) {
+        super(costumer);
+        this.interest = interest;
+    }
 
 }
