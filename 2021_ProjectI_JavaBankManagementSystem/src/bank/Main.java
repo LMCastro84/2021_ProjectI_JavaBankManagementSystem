cadf1beb8722dc94679efda55e5202ac64cb62aa
Luis Miguel Castro <58310131+LMCastro84@users.noreply.github.com>
2021-01-29 21:54:39 +0000

    Testing creation 2nd costumer

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/Main.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/Main.java
@@ -20,5 +20,17 @@ public class Main {
         dbtCard.setCurrentAccount(currAcct);
 
         cost1.showClientInfo();
+        
+        
+        Costumer cost2 = new Costumer("Silva", "986966418", "marisa@gmail.com", "BNP");
+        CurrentAccount currAcct2 = new CurrentAccount(cost2);
+        DebitCard dbtCard2 = new DebitCard(cost2);
+        cost2.setCurrentAccount(currAcct2);
+        cost2.setDebitCard(dbtCard2);
+        currAcct2.setDebitCard(dbtCard2);
+        dbtCard2.setCurrentAccount(currAcct2);
+        System.out.println("");
+
+        cost2.showClientInfo();
     }
 }
