cf12f0d3c737f55698cca1128264c1bf80406349
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-01-29 23:51:19 +0000

    extends Transfer

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/movements/Transfer.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/movements/Transfer.java
@@ -1,9 +1,15 @@
 package bank.movements;
 
+import bank.accounts.CurrentAccount;
+
 /**
  *
  * @author Castro
  */
 public interface Transfer {
-    
+
+    /**
+     * transfers between two current accounts
+     */
+    public void normalTransfer();
 }
