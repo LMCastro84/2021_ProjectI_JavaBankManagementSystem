322c22fe821043b594c3ed219e5a32110cf21384
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-01-29 23:34:56 +0000

    constructor + comments

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/accounts/TermAccount.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/accounts/TermAccount.java
@@ -1,5 +1,7 @@
 package bank.accounts;
 
+import bank.customers.Customer;
+
 /**
  *
  * @author Castro
@@ -10,5 +12,17 @@ public class TermAccount extends Account {
      * ammount deposited to allow the term account to be created
      */
     private double termDeposit;
+
+    /**
+     * constructor
+     *
+     * @param costumer the owner of the account
+     * @param termDeposit of the term account
+     */
+    public TermAccount(Customer costumer, double termDeposit) {
+        super(costumer);
+        id++;
+        this.termDeposit = termDeposit;
+    }
 
 }
