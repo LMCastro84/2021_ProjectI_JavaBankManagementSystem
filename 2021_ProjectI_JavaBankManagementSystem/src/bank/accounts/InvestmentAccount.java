5e5fbd466824b5d562ee453329a1ed98639cbd8e
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-01-29 22:26:50 +0000

    associatedCost + comment; constructor + comments

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/accounts/InvestmentAccount.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/accounts/InvestmentAccount.java
@@ -1,9 +1,26 @@
 package bank.accounts;
 
+import bank.customers.Customer;
+
 /**
  *
  * @author Castro
  */
-public class InvestmentAccount extends Account{
-    
+public class InvestmentAccount extends Account {
+
+    /**
+     * the associated cost to maintain the account
+     */
+    private double associatedCost;
+
+    /**
+     * constructor
+     *
+     * @param costumer the owner of the account
+     */
+    public InvestmentAccount(Customer costumer) {
+        super(costumer);
+        this.associatedCost = 5;
+    }
+
 }
