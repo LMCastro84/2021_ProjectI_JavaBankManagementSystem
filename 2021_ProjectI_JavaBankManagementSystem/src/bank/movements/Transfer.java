6f1c8ca1c0830142895a910d89b4f4be7ee2e7d8
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-01-29 23:52:18 +0000

    difAcctTypeTransfer() + comment

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/movements/Transfer.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/movements/Transfer.java
@@ -12,4 +12,9 @@ public interface Transfer {
      * transfers between two current accounts
      */
     public void normalTransfer();
+
+    /**
+     * transfers between diferent type of accounts
+     */
+    public void difAcctTypeTransfer();
 }
