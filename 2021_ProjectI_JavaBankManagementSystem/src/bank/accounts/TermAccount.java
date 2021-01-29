cc851c391e792d616ca869e16ae372fa5eab6f1e
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-01-29 23:30:22 +0000

    extends Account; termDeposit + comment

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/accounts/TermAccount.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/accounts/TermAccount.java
@@ -4,6 +4,11 @@ package bank.accounts;
  *
  * @author Castro
  */
-public class TermAccount {
-    
+public class TermAccount extends Account {
+
+    /**
+     * ammount deposited to allow the term account to be created
+     */
+    private double termDeposit;
+
 }
