76ccd8165645816e1e51e7772a2a0f7d719142c8
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-01-30 00:19:50 +0000

    @Override normal(Transfer() + difAcctTypeTransfer() + comments

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/accounts/TermAccount.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/accounts/TermAccount.java
@@ -1,12 +1,13 @@
 package bank.accounts;
 
 import bank.customers.Customer;
+import bank.movements.Transfer;
 
 /**
  *
  * @author Castro
  */
-public class TermAccount extends Account {
+public class TermAccount extends Account implements Transfer {
 
     /**
      * ammount deposited to allow the term account to be created
@@ -23,6 +24,20 @@ public class TermAccount extends Account {
         super(costumer);
         id++;
         this.termDeposit = termDeposit;
+    }
+
+    /**
+     * transfers between two current accounts
+     */
+    @Override
+    public void normalTransfer() {
+    }
+
+    /**
+     * transfers between diferent type of accounts
+     */
+    @Override
+    public void difAcctTypeTransfer() {
     }
 
 }
