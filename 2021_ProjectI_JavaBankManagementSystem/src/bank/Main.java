8a50377559feab5924a6deb55dc31fcbbdbc9bf1
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-01-31 22:58:47 +0000

    corrections

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/Main.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/Main.java
@@ -1,8 +1,10 @@
 package bank;
 
 import bank.accounts.CurrentAccount;
+import bank.cards.CreditCard;
 import bank.cards.DebitCard;
 import bank.customers.Customer;
+import bank.customers.VIPCustomer;
 
 /**
  *
@@ -18,12 +20,12 @@ public class Main {
         cost1.setDebitCard(dbtCard);
         currAcct.setDebitCard(dbtCard);
         dbtCard.setCurrentAccount(currAcct);
-
-        cost1.showClientInfo();
+        CreditCard creditCard1 = new CreditCard(cost1, 50);
+        creditCard1.setCurrentAccount(currAcct);
 
         System.out.println("");
         System.out.println("");
-        Customer cost2 = new Customer("Silva", "986966418", "marisa@gmail.com", "BNP");
+        Customer cost2 = new VIPCustomer("Silva", "986966418", "marisa@gmail.com", "BNP", "Castro Manager");
         CurrentAccount currAcct2 = new CurrentAccount(cost2);
         DebitCard dbtCard2 = new DebitCard(cost2);
         cost2.setCurrentAccount(currAcct2);
