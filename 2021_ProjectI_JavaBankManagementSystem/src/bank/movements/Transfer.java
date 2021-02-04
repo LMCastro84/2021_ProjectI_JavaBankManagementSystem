20969f5a11b69f9eee78c12e29c57b93cbf323db
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-02-04 23:22:53 +0000

    comment normalTransfer()

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/movements/Transfer.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/movements/Transfer.java
@@ -10,6 +10,9 @@ public interface Transfer {
 
     /**
      * transfers between two current accounts
+     *
+     * @param amount quantity of money to be transfered
+     * @param numRecAcct account id of the Account that will recieve the amount
      */
     default public void normalTransfer(double amount, int numRecAcct) {
     }
