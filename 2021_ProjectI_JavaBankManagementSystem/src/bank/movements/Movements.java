97dacca9f90c24c3e5d904f5de6d0d34ae4ba56a
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-02-04 23:20:24 +0000

    comment withdraw()

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/movements/Movements.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/movements/Movements.java
@@ -8,6 +8,8 @@ public interface Movements extends Transfer, Deposit {
 
     /**
      * withdraw ammount on an account
+     *
+     * @param amount quantity of money to be withdrawn
      */
     default public void withdraw(double amount) {
     }
