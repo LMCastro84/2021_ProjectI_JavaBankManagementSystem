2164e66d2b935184976355ab2c984900903f5f1c
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-02-16 23:33:28 +0000

    showMainMenu() + comment

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/Menu.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/Menu.java
@@ -6,4 +6,17 @@ package bank;
  */
 public class Menu {
 
+    /**
+     * shows the welcome/main menu
+     */
+    public static void showMainMenu(){
+        System.out.println("Welcome!");
+        System.out.println("Please enter an option:");
+        System.out.println("1 - Create New Client;");
+        System.out.println("2 - List all Clients;");
+        System.out.println("3 - Client options;");
+        System.out.println("4 - Select a Card;");
+        System.out.println("5 - Advence one month;");
+        System.out.println("6 - Exit.");
+    }
 }
