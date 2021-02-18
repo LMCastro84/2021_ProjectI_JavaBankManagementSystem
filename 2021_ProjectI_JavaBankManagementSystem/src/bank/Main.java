37f6be8c2e47a995c7821be865fb65212ebd6bdc
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-02-18 22:50:48 +0000

    edit case 1 - case 1 (new regular customer)

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/Main.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/Main.java
@@ -41,6 +41,15 @@ public class Main {
                             System.out.print("Insert Customer's profession: ");
                             profession = in.nextLine();
                             Customer newCustomer = new Customer(name, phone, email, profession);
+                            CurrentAccount currAcct = new CurrentAccount(newCustomer);
+                            DebitCard dbtCard = new DebitCard(newCustomer);
+                            newCustomer.setCurrentAccount(currAcct);
+                            newCustomer.setDebitCard(dbtCard);
+                            currAcct.setDebitCard(dbtCard);
+                            dbtCard.setCurrentAccount(currAcct);
+                            CreditCard creditCard = new CreditCard(newCustomer, 50);
+                            creditCard.setCurrentAccount(currAcct);
+                            newCustomer.showClientInfo();
                             break;
                         case 2:
                             break;
@@ -65,29 +74,28 @@ public class Main {
             }
         } while (option != 6);
 
-        Customer cost1 = new Customer("Castro", "936960352", "castro@gmail.com", "Java Dev");
-        CurrentAccount currAcct = new CurrentAccount(cost1);
-        DebitCard dbtCard = new DebitCard(cost1);
-        cost1.setCurrentAccount(currAcct);
-        cost1.setDebitCard(dbtCard);
-        currAcct.setDebitCard(dbtCard);
-        dbtCard.setCurrentAccount(currAcct);
-        CreditCard creditCard1 = new CreditCard(cost1, 50);
-        creditCard1.setCurrentAccount(currAcct);
-        cost1.showClientInfo();
-
-        System.out.println("");
-        System.out.println("");
-        Customer cost2 = new VIPCustomer("Silva", "986966418", "marisa@gmail.com", "BNP", "Castro Manager");
-        CurrentAccount currAcct2 = new CurrentAccount(cost2);
-        DebitCard dbtCard2 = new DebitCard(cost2);
-        cost2.setCurrentAccount(currAcct2);
-        cost2.setDebitCard(dbtCard2);
-        currAcct2.setDebitCard(dbtCard2);
-        dbtCard2.setCurrentAccount(currAcct2);
-        cost2.showClientInfo();
-
-        System.out.println("\nTests:");
+//        Customer cost1 = new Customer("Castro", "936960352", "castro@gmail.com", "Java Dev");
+//        CurrentAccount currAcct = new CurrentAccount(cost1);
+//        DebitCard dbtCard = new DebitCard(cost1);
+//        cost1.setCurrentAccount(currAcct);
+//        cost1.setDebitCard(dbtCard);
+//        currAcct.setDebitCard(dbtCard);
+//        dbtCard.setCurrentAccount(currAcct);
+//        CreditCard creditCard1 = new CreditCard(cost1, 50);
+//        creditCard1.setCurrentAccount(currAcct);
+//        cost1.showClientInfo();
+//        System.out.println("");
+//        System.out.println("");
+//        Customer cost2 = new VIPCustomer("Silva", "986966418", "marisa@gmail.com", "BNP", "Castro Manager");
+//        CurrentAccount currAcct2 = new CurrentAccount(cost2);
+//        DebitCard dbtCard2 = new DebitCard(cost2);
+//        cost2.setCurrentAccount(currAcct2);
+//        cost2.setDebitCard(dbtCard2);
+//        currAcct2.setDebitCard(dbtCard2);
+//        dbtCard2.setCurrentAccount(currAcct2);
+//        cost2.showClientInfo();
+//
+//        System.out.println("\nTests:");
 
     }
 }
