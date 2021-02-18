0f5f8006e00ed0caf63277aa0012bf5685f47d9b
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-02-18 21:05:11 +0000

    showCustomerMenu()

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/Menu.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/Menu.java
@@ -9,18 +9,20 @@ public class Menu {
     /**
      * shows the welcome/main menu
      */
-    public static void showMainMenu(){
+    public static void showMainMenu() {
         System.out.println("Welcome!");
         System.out.println("Please enter an option:");
-        System.out.println("1 - Create New Client;");
+        System.out.println("1 - Create New Customer;");
         System.out.println("2 - List all Clients;");
         System.out.println("3 - Client options;");
         System.out.println("4 - Select a Card;");
         System.out.println("5 - Advance one month;");
         System.out.println("6 - Exit.");
     }
-    
-    public static void showClientMenu(){
-            System.out.println("Test");
+
+    public static void showCustomerMenu() {
+        System.out.println("1 - New Regular Customer;");
+        System.out.println("2 - New VIP Customer;");
+        System.out.println("3 - Exit.");
     }
 }
