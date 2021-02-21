4e6cd095722e378d40192bce04920f4db8a19d11
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-02-21 22:53:57 +0000

    corrections

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/Main.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/Main.java
@@ -76,7 +76,6 @@ public class Main {
                             CreditCard vipCreditCard = new CreditCard(newVIPCustomer, 50);
                             vipCreditCard.setCurrentAccount(vipCurrAcct);
                             newVIPCustomer.showClientInfo();
-
                             break;
                         case 3:
                             break;
@@ -98,28 +97,5 @@ public class Main {
                     System.out.println("Option 6;");
             }
         } while (option != 6);
-
-//        Customer cost1 = new Customer("Castro", "936960352", "castro@gmail.com", "Java Dev");
-//        CurrentAccount currAcct = new CurrentAccount(cost1);
-//        DebitCard dbtCard = new DebitCard(cost1);
-//        cost1.setCurrentAccount(currAcct);
-//        cost1.setDebitCard(dbtCard);
-//        currAcct.setDebitCard(dbtCard);
-//        dbtCard.setCurrentAccount(currAcct);
-//        CreditCard creditCard1 = new CreditCard(cost1, 50);
-//        creditCard1.setCurrentAccount(currAcct);
-//        cost1.showClientInfo();
-//        System.out.println("");
-//        System.out.println("");
-//        Customer cost2 = new VIPCustomer("Silva", "986966418", "marisa@gmail.com", "BNP", "Castro Manager");
-//        CurrentAccount currAcct2 = new CurrentAccount(cost2);
-//        DebitCard dbtCard2 = new DebitCard(cost2);
-//        cost2.setCurrentAccount(currAcct2);
-//        cost2.setDebitCard(dbtCard2);
-//        currAcct2.setDebitCard(dbtCard2);
-//        dbtCard2.setCurrentAccount(currAcct2);
-//        cost2.showClientInfo();
-//
-//        System.out.println("\nTests:");
     }
 }
