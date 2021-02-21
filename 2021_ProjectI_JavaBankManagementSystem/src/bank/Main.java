db20ad43342fd182c3ca51053369b3f06600352b
Luis Miguel Castro <luis.mbscastro@gmail.com>
2021-02-21 22:52:12 +0000

    edit case1 - case 2

--- 2021_ProjectI_JavaBankManagementSystem/src/bank/Main.java
+++ 2021_ProjectI_JavaBankManagementSystem/src/bank/Main.java
@@ -14,6 +14,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        String name;
+        String phone;
+        String email;
+        String profession;
+        String accountManager;
         int option = 0;
         Scanner in = new Scanner(System.in);
         do {
@@ -27,10 +32,6 @@ public class Main {
                     option = in.nextInt();
                     switch (option) {
                         case 1:
-                            String name,
-                             phone,
-                             email,
-                             profession;
                             System.out.println("New Regular Customer");
                             System.out.print("Insert Customer's name: ");
                             name = in.nextLine();
@@ -53,11 +54,7 @@ public class Main {
                             newCustomer.showClientInfo();
                             break;
                         case 2:
-                            String name,
-                             phone,
-                             email,
-                             profession;
-                            System.out.println("New Regular Customer");
+                            System.out.println("New VIP Customer");
                             System.out.print("Insert Customer's name: ");
                             name = in.nextLine();
                             name = in.nextLine();
@@ -67,16 +64,18 @@ public class Main {
                             email = in.nextLine();
                             System.out.print("Insert Customer's profession: ");
                             profession = in.nextLine();
-                            Customer newCustomer = new Customer(name, phone, email, profession);
-                            CurrentAccount currAcct = new CurrentAccount(newCustomer);
-                            DebitCard dbtCard = new DebitCard(newCustomer);
-                            newCustomer.setCurrentAccount(currAcct);
-                            newCustomer.setDebitCard(dbtCard);
-                            currAcct.setDebitCard(dbtCard);
-                            dbtCard.setCurrentAccount(currAcct);
-                            CreditCard creditCard = new CreditCard(newCustomer, 50);
-                            creditCard.setCurrentAccount(currAcct);
-                            newCustomer.showClientInfo();
+                            System.out.println("Insert Customer's Account Manager");
+                            accountManager = in.nextLine();
+                            VIPCustomer newVIPCustomer = new VIPCustomer(name, phone, email, profession, accountManager);
+                            CurrentAccount vipCurrAcct = new CurrentAccount(newVIPCustomer);
+                            DebitCard vipDbtCard = new DebitCard(newVIPCustomer);
+                            newVIPCustomer.setCurrentAccount(vipCurrAcct);
+                            newVIPCustomer.setDebitCard(vipDbtCard);
+                            vipCurrAcct.setDebitCard(vipDbtCard);
+                            vipDbtCard.setCurrentAccount(vipCurrAcct);
+                            CreditCard vipCreditCard = new CreditCard(newVIPCustomer, 50);
+                            vipCreditCard.setCurrentAccount(vipCurrAcct);
+                            newVIPCustomer.showClientInfo();
 
                             break;
                         case 3:
